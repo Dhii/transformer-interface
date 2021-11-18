11764806103bf29bf660a527711d660976e43dd1
Anton Ukhanev <xedin.unknown@gmail.com>
2021-11-18 07:45:08 +0100

    Add generics

--- src/TransformerAwareInterface.php
+++ src/TransformerAwareInterface.php
@@ -6,11 +6,16 @@ namespace Dhii\Transformer;
 
 /**
  * Something that exposes a transformer.
+ *
+ * @template Input
+ * @template Output
  */
 interface TransformerAwareInterface
 {
     /**
      * Retrieves the transformer associated with this instance.
+     *
+     * @psalm-return TransformerInterface<\Input, \Output>
      */
     public function getTransformer(): TransformerInterface;
 }
